cd715132a4c4f9d22096ae6648f74803b6d26196
Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>
2019-11-03 12:20:34 +0000

    Update interpolation
    Signed-off-by: Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>

--- badge/handler.js
+++ badge/handler.js
@@ -12,17 +12,17 @@ module.exports = (event, context) => {
         let owner = q.owner;
 
         if (!repo || !owner) {
-            return context.fail("Give a repo and owner in the querystring");
+            return context.fail(`Give a repo and owner in the querystring: ${event.query}`);
         }
 
-        axios.get(`https://raw.githubusercontent.com/{owner}/{repo}/master/.DEREK.yml`)
+        axios.get(`https://raw.githubusercontent.com/${owner}/${repo}/master/.DEREK.yml`)
         .then(function (response) {
             if(response.status == 200) {
                 let doc = YAML.parse(response.data)
                 let numFeatures = doc.features.length
 
                 return context.status(307).
-                headers({"Location": `https://img.shields.io/badge/derek-{numFeatures}-features.svg`})
+                headers({"Location": `https://img.shields.io/badge/derek-${numFeatures}-features.svg`})
             } 
         })
     }
